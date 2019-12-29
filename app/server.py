8da781976c5b3ed05b13c486e08fe392e1ea2fb5
lpop <laur.pop.91@gmail.com>
2019-12-29 22:50:34 +0100

    Add redirect for 404

--- app/server.py
+++ app/server.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from utils import get_result
 
 app = Flask(__name__)
@@ -12,11 +12,11 @@ def home():
 
 
 @app.route('/<algorithm_name>/', methods=["GET", "POST"])
-def algorithm_calculator(algorithm_name, result='', exec_time=''):
+def algorithm_calculator(algorithm_name):
     if request.method == 'GET' and algorithm_name in ("Fibonacci", "Ackermann", "Factorial"):
-        return render_template('algorithm.html', name=algorithm_name, result=result, exec_time=exec_time)
+        return render_template('algorithm.html', name=algorithm_name)
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
         user_input = [request.form.get('parameter')]
         if algorithm_name == 'Ackermann':
             user_input.append(request.form.get('second-parameter'))
@@ -27,6 +27,9 @@ def algorithm_calculator(algorithm_name, result='', exec_time=''):
             return render_template('algorithm.html', name=algorithm_name, error_message=result)
 
         return render_template('algorithm.html', name=algorithm_name, result=result, exec_time=exec_time)
+
+    else:
+        return redirect("/"), 404, {"Refresh": "3; url=/"}
 
 
 if __name__ == "__main__":
