42f75e9aef0fae87cc0b5a5e36d05b0cca2fa731
lpop <laur.pop.91@gmail.com>
2019-12-29 21:48:35 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -28,5 +28,4 @@ def algorithm_calculator(algorithm_name, result='', exec_time=''):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
