c1d3011d027be3ea442ce193c21aaedf8751624c
lpop <laur.pop.91@gmail.com>
2019-12-30 00:12:32 +0100

    Fix bug with specifying host for Docker image

--- app/server.py
+++ app/server.py
@@ -33,4 +33,4 @@ def algorithm_calculator(algorithm_name):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
