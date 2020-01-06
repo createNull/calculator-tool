c9ac325da3a9132b0167cb5571e733c8a27ea573
lpop <laur.pop.91@gmail.com>
2020-01-06 22:39:42 +0100

    Add timeout and handle HTTP errors

--- app/server.py
+++ app/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, abort
+from timeout_decorator import timeout, TimeoutError
 from utils import run_algorithm
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ def home():
 
 
 @app.route('/<algorithm_name>/', methods=["GET", "POST"])
+@timeout(10)
 def algorithm_calculator(algorithm_name: str):
     if request.method == 'GET' and algorithm_name in ("Fibonacci", "Ackermann", "Factorial"):
         return render_template('algorithm.html', name=algorithm_name)
@@ -25,8 +27,22 @@ def algorithm_calculator(algorithm_name: str):
         return render_template('algorithm.html', name=algorithm_name, error_message=result)
 
     else:
-        return redirect('/'), 404, {"Refresh": "3; url=/"}
+        abort(404)
+
+
+@app.errorhandler(404)
+def handle_file_not_found(e):
+    error_code = '404 File Not Found'
+    error_text = "Oops, the page you're looking for doesn't exist."
+    return render_template('error.html', error_code=error_code, error_text=error_text)
+
+
+@app.errorhandler(TimeoutError)
+def handle_timeout(e):
+    error_code = '500 Request Timed Out'
+    error_text = 'Looks like the server is taking too long to respond, please try again later.'
+    return render_template('error.html', error_code=error_code, error_text=error_text)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
