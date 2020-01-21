942348989fc7449f735c1f1872159d9e2494223a
lpop <laur.pop.91@gmail.com>
2020-01-21 12:42:58 +0100

    Return error network msg

--- app/server.py
+++ app/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, abort
 from timeout_decorator import timeout, TimeoutError
 
-from app.utils import run_algorithm
+from utils import run_algorithm
 
 app = Flask(__name__)
 
@@ -36,7 +36,7 @@ def handle_file_not_found(e):
     error_code = '404'
     error_name = 'File Not Found'
     error_text = "Oops, the page you're looking for doesn't exist."
-    return render_template('error.html', error_code=error_code, error_name=error_name, error_text=error_text)
+    return render_template('error.html', error_code=error_code, error_name=error_name, error_text=error_text), 404
 
 
 @app.errorhandler(TimeoutError)
@@ -44,7 +44,7 @@ def handle_timeout(e):
     error_code = '408'
     error_name = 'Request Timeout'
     error_text = 'Looks like the server is taking too long to respond, please try again later.'
-    return render_template('error.html', error_code=error_code, error_name=error_name, error_text=error_text)
+    return render_template('error.html', error_code=error_code, error_name=error_name, error_text=error_text), 408
 
 
 if __name__ == "__main__":
